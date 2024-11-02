5d1fd6788153d13c1f487f4e22164722adaaee61
Nick Land <land.d.nicholas@gmail.com>
2024-11-02 00:54:05 +0000

    feat(config): :heavy_plus_sign: add storybook to project
    This commit also includes vitest.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 declare module "@remix-run/node" {
@@ -8,17 +8,15 @@ declare module "@remix-run/node" {
   }
 }
 
+const isStorybook = process.argv[1]?.includes("storybook");
+
 export default defineConfig({
   plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
-    tsconfigPaths(),
+    !process.env.VITEST && !isStorybook && remix(),
   ],
+    test: {
+    environment: "happy-dom",
+    // Additionally, this is to load ".env.test" during vitest
+    env: loadEnv("test", process.cwd(), ""),
+  },
 });
